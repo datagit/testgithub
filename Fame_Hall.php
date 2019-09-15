0340f4c0e283bef33253d6044e532f06af7cba65
Dat Dao <datagit@yahoo.com>
2019-09-15 13:19:12 +0700

    second

--- Fame_Hall.php
+++ Fame_Hall.php
@@ -1,9 +1,17 @@
 <?php
+
+/**
+ * Class Fame_Hall
+ * constants
+ * support tool
+ * cache
+ * frontend
+ */
 class Fame_Hall {
 	const Event_Type_Amusement = 1;
 	const Ranking_Type_Amusement = 1;
 
-	private $_config = array(
+	private static $_config = array(
 		// event_type
 		Fame_Hall::Event_Type_Amusement => array(
 			// ranking_type
@@ -11,16 +19,24 @@ class Fame_Hall {
 		),
 	);
 
-	private $_config_table_identified_list = array(
-		Fame_Hall::Event_Type_Amusement => 'namespace_asusement_%d',
+	private static $_config_table_identified_list = array(
+		Fame_Hall::Event_Type_Amusement => 'namespace_amusement_%d',
 	);
+
+	public function createTables($event_type)
+    {
+        foreach (Fame_Hall::$_config[$event_type] as $ranking_type) {
+            // call create table
+
+        }
+    }
 
 	public function getTableIdentified($event_type, $event_id)
 	{
 		return sprintf(Fame_Hall::$_config_table_identified_list[$event_type], $event_id);
 	}
 
-	public function getNamespaceeByTableIdentified($table_identified)
+	public function getNamespaceByTableIdentified($table_identified)
 	{
 		return $namespace = ($table_identified);
 	}
@@ -33,7 +49,7 @@ class Fame_Hall {
 	public function getDataList($event_type, $event_id)
 	{
 		$table_identified = $this->getTableIdentified($event_type, $event_id);
-		$namespace = $this->getNamespaceeByTableIdentified($table_identified);
+		$namespace = $this->getNamespaceByTableIdentified($table_identified);
 		$data_ranking = $this->getDataRankingBy($namespace, $event_id);
 		// execute the logical get data list
 
